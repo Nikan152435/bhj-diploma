e4f083e27b1ad9eeac210c7f62a657a1acc7181b
NikaN <NikaN7602@gmail.com>
2023-11-22 02:20:20 +0100

    README.md

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,39 +3,39 @@
  * на сервер.
  * */
 const createRequest = ({url, data = {}, method, callback = () => {}}) => {
-  const xhr = new XMLHttpRequest();
-  const formData = new FormData();
-  xhr.responseType = 'json';
-
-  if (method == 'GET') {
-  //  https://example.com?mail=ivan@biz.pro&password=odinodin
-    for (let key in data) {
-      url += `?${key}=${data[key]}&`;
-    }
-  } else {
-    for (key in data) {
-      formData.append(key, data[key]);
-    }
-  }
-
-  try {
-    xhr.open(method, url);
+    const xhr = new XMLHttpRequest();
+    const formData = new FormData();
+    xhr.responseType = 'json';
+  
     if (method == 'GET') {
-      xhr.send();
+    //  https://example.com?mail=ivan@biz.pro&password=odinodin
+      for (let key in data) {
+        url += `?${key}=${data[key]}&`;
+      }
     } else {
-      xhr.send(formData);
+      for (key in data) {
+        formData.append(key, data[key]);
+      }
     }
-  }
-  catch (err) {
-    callback(err);
-  }
-
-  xhr.onload = () => {
-    if (xhr.status != 200) {
+  
+    try {
+      xhr.open(method, url);
+      if (method == 'GET') {
+        xhr.send();
+      } else {
+        xhr.send(formData);
+      }
+    }
+    catch (err) {
       callback(err);
-      console.log(xhr.status, xhr.responseText);
-    } else {
-      callback(null, xhr.response);
     }
-  }
-};
+  
+    xhr.onload = () => {
+      if (xhr.status != 200) {
+        callback(err);
+        console.log(xhr.status, xhr.responseText);
+      } else {
+        callback(null, xhr.response);
+      }
+    }
+  };
--- public/js/ui/forms/CreateTransactionForm.js
+++ public/js/ui/forms/CreateTransactionForm.js
@@ -1,37 +1,57 @@
-const Account = require('./Account');
-const Transaction = require('./Transaction');
-
+/**
+ * Класс CreateTransactionForm управляет формой
+ * создания новой транзакции
+ * */
 class CreateTransactionForm extends AsyncForm {
+  /**
+   * Вызывает родительский конструктор и
+   * метод renderAccountsList
+   * */
   constructor(element) {
     super(element);
     this.renderAccountsList();
   }
 
+  /**
+   * Получает список счетов с помощью Account.list
+   * Обновляет в форме всплывающего окна выпадающий список
+   * */
   renderAccountsList() {
-    const accountsList = this.element.querySelector(".accounts-select");
-    Account.list(User.current(), (err, response) => {
-      if (response && response.data) {
-        accountsList.innerHTML = "";
-        response.data.forEach((account) => {
-          const option = document.createElement("option");
-          option.value = account.id;
-          option.textContent = account.name;
-          accountsList.appendChild(option);
-        });
-      }
-    });
+    const selectForm = document.querySelectorAll('.accounts-select');
+    if (User.current()) {
+      Account.list(User.current(), (err, response) => {
+        if (response && response.success) {
+          [...selectForm].forEach(select => {
+            select.innerHTML = '';
+            response.data.forEach(data => {
+              const option = document.createElement('option');
+              option.value = `${data.id}`;
+              option.textContent = `${data.name}`;
+              select.append(option);
+            });
+          });
+        } else {
+          console.log('Ошибка при получении списка счетов', response.error);
+        }
+      });
+    }
   }
 
+  /**
+   * Создаёт новую транзакцию (доход или расход)
+   * с помощью Transaction.create. По успешному результату
+   * вызывает App.update(), сбрасывает форму и закрывает окно,
+   * в котором находится форма
+   * */
   onSubmit(data) {
     Transaction.create(data, (err, response) => {
       if (response && response.success) {
-        this.element.reset();
-        const modal = this.element.closest(".modal");
-        if (modal) {
-          const modalObject = App.getModal(modal.dataset.modalId);
-          modalObject.close();
-        }
         App.update();
+        this.element.reset();
+        App.getModal('newIncome').close();
+        App.getModal('newExpense').close();
+      } else {
+        console.log('Ошибка регистрации', response.error);
       }
     });
   }
